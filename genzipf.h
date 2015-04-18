45e5f7dbe2244ef04e9c3c34b60051a8bf729880
Deep Desai <deepdesai.nitt@gmail.com>
2015-04-18 12:21:33 -0500

    remove all genzipf

--- genzipf.h
+++
@@ -1,3 +0,0 @@
-//----- Function prototypes -------------------------------------------------
-int      zipf(double alpha, int n);  // Returns a Zipf random variable
-double   rand_val(int seed);         // Jain's RNG
