5405e9587ffbfc7a0350ff667bbd1825409ddedd
Deep Desai <deepdesai.nitt@gmail.com>
2015-04-18 11:59:44 -0500

    add genzip.h and genzipf.cpp

---
+++ genzipf.h
@@ -0,0 +1,3 @@
+//----- Function prototypes -------------------------------------------------
+int      zipf(double alpha, int n);  // Returns a Zipf random variable
+double   rand_val(int seed);         // Jain's RNG
