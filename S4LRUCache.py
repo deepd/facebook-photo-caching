5d7f302afa1fade25b519459baa80eb50f627d52
Deep Desai <deepdesai.nitt@gmail.com>
2015-05-08 18:02:41 -0500

    fix logical errors

--- S4LRUCache.py
+++ S4LRUCache.py
@@ -25,18 +25,18 @@ class S4LRUCache:
             except KeyError:
                 try:
                     value = self.cache3.pop(key)
-                    self.setCache4(key)
+                    self.set(key)
                     return value
                 except KeyError:
                     try:
                         value = self.cache4.pop(key)
-                        self.setCache4(key)
+                        self.set(key)
                         return value
                     except KeyError:
                         return -1
 
 
-    def set(self, key):
+    def setCache1(self, key):
         if len(self.cache1) >= self.capacity1:
             self.cache1.popitem(last=False)
         self.cache1[key] = key
@@ -44,7 +44,7 @@ class S4LRUCache:
     def setCache2(self, key):
         if len(self.cache2) >= self.capacity2:
             [k,v] = self.cache2.popitem(last=False)
-            self.set(k)
+            self.setCache1(k)
         self.cache2[key] = key
 
     def setCache3(self, key):
@@ -53,7 +53,7 @@ class S4LRUCache:
             self.setCache2(k)
         self.cache3[key] = key
 
-    def setCache4(self, key):
+    def set(self, key):
         if len(self.cache4) >= self.capacity4:
             [k,v] = self.cache4.popitem(last=False)
             self.setCache3(k)
